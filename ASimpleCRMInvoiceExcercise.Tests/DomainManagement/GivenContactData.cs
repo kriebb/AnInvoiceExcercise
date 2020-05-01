0aea1db6ec8dc46c4542bfec38bae3fb636b92e8
Kristof Riebbels <kristof.riebbels@recomatics.com>
2020-05-01 11:29:38 +0200

    Added ContactData

--- ASimpleCRMInvoiceExcercise.Tests/DomainManagement/GivenContactData.cs
+++ ASimpleCRMInvoiceExcercise.Tests/DomainManagement/GivenContactData.cs
@@ -1,3 +1,4 @@
+using Bogus;
 using Xunit;
 
 namespace ASimpleCRMInvoiceExcercise.Tests
@@ -6,15 +7,28 @@ namespace ASimpleCRMInvoiceExcercise.Tests
     {
         public class ShouldDefine
         {
+            private ContactInfo _contactInfo;
+
+            public ShouldDefine()
+            {
+                _contactInfo = new ContactInfo();
+
+            }
             [Fact]
             public void AnEmail()
             {
+                _contactInfo.Type = ContactInfo.Email;
+                _contactInfo.Value = new Faker().Internet.Email();
+                Assert.Equal(ContactInfo.Email, _contactInfo.Type);
 
             }
             [Fact]
             public void ACellNumber()
             {
+                _contactInfo.Type = ContactInfo.CellNumber;
+                _contactInfo.Value = new Faker().Person.Phone;
 
+                Assert.Equal(ContactInfo.CellNumber, _contactInfo.Type);
             }
         }
 
--- Backend.API.Domain/ContactInfo.cs
+++ Backend.API.Domain/ContactInfo.cs
@@ -6,5 +6,7 @@ namespace ASimpleCRMInvoiceExcercise.Tests
     {
         public string Type { get; set; }
         public string Value { get; set; }
+        public static string Email { get; set; } = "Email";
+        public static string CellNumber { get; set; } = "CellNumber";
     }
 }
