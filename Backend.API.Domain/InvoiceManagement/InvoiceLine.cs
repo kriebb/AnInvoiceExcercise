dbfdffb0dc25f95a7b22e8aaf302b053d48732f1
Kristof Riebbels <kristof.riebbels@recomatics.com>
2020-05-01 17:52:10 +0200

    added invoiceLine properties and tests

--- Backend.API.Domain/InvoiceManagement/InvoiceLine.cs
+++ Backend.API.Domain/InvoiceManagement/InvoiceLine.cs
@@ -4,6 +4,8 @@ namespace ASimpleCRMInvoiceExcercise.Tests
 {
     public class InvoiceLine:Entity
     {
-
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal TotalPrice { get; set; }
     }
 }
